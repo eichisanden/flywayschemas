070c0525661051453e92d99f8b6cd19b1cb007b8
eichisanden <eichisanden@gmail.com>
2016-06-05 09:37:43 +0900

    Fix test code.

--- src/test/groovy/com/github/flywaySchemas/FlywaySchemasPluginSpec.groovy
+++ src/test/groovy/com/github/flywaySchemas/FlywaySchemasPluginSpec.groovy
@@ -1,21 +1,29 @@
 package com.github.flywaySchemas
 
+import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class FlywaySchemasPluginSpec extends Specification {
 
-    def "apply() should load the plugin"() {
-        given:
-        def project = ProjectBuilder.builder().build()
+    Project project
+    def setup() {
+        project = ProjectBuilder.builder().build()
+        project.apply plugin: 'com.github.flyway-schemas'
+    }
 
-        when:
-        project.with {
-            apply plugin: 'com.github.flyway-schemas'
-        }
-
-        then:
+    def "project has this plugin"() {
+        expect:
         project.plugins.hasPlugin(FlywaySchemasPlugin)
     }
 
+    def "check if task are present"() {
+        expect:
+        project.tasks.findByName('flywayClean') != null
+        project.tasks.findByName('flywayInfo') != null
+        project.tasks.findByName('flywayBaseline') != null
+        project.tasks.findByName('flywayMigrate') != null
+        project.tasks.findByName('flywayRepair') != null
+        project.tasks.findByName('flywayValidate') != null
+    }
 }
